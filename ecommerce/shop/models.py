51081b581a8715c7e06077125c6ca4d3c4a669e8
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-06 10:47:33 +0300

    add two models

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+# from django.urls import reverse
+from django.utils.text import slugify
+
+alphabet = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
+            'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+            'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ы': 'i', 'э': 'e',
+            'ю': 'yu',
+            'я': 'ya'}
+
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+
+    def __str__(self):
+        return self.category_name
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    characteristics = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.URLField()
+    slug = models.SlugField(max_length=100)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = self.slugify_rus(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+    def slugify_rus(self, s):
+        """
+        Overriding django slugify that allows to use russian words as well.
+        """
+        return slugify(''.join(alphabet.get(w, w) for w in s.lower()))
+
+    def __str__(self):
+        return self.name
