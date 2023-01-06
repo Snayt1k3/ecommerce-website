c61d99af61e3bf153511484446ff9111d709e43d
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-06 12:55:59 +0300

    add urls on navbar

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view())
+    path('', views.HomeView.as_view()),
+    path('<str:category>/', views.prod_by_category, name='pr_by_cat')
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -11,7 +11,19 @@ from .models import Category, Product
 
 
 class HomeView(ListView):
-    paginate_by = 20
+    paginate_by = 16
     model = Product
     context_object_name = 'products'
     template_name = 'shop/base.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+def prod_by_category(request, category):
+    cat = Category.objects.get(category_name=category)
+    products = Product.objects.filter(category=cat)
+    return render(request, 'shop/base.html', context={
+        'products': products,
+        'categories': Category.objects.all(),
+    })
