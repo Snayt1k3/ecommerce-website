c7c9342c6abe1939c8b56e666d74d5f79b3b395e
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-06 16:21:36 +0300

    add url

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
     path('<str:category>/', views.prod_by_category, name='pr_by_cat'),
     path('product/<slug:slug>/', views.ProductView.as_view(), name='one_pr'),
 ]
