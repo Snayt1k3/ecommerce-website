16e6ffc11c064e000e4827f11a27fd5b61786903
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-10 13:18:31 +0300

    add css for form

--- ecommerce/shop/forms.py
+++ ecommerce/shop/forms.py
@@ -4,12 +4,16 @@ from django.contrib.auth.models import User
 
 
 class UserLogForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}), label='Логин')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Ваш Email'}))
 
 
 class UserRegForm(UserCreationForm):
     model = User
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Повторите Пароль'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}), label='Логин')
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Ваш Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль')
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Повторите Пароль'}),
+                                label='Повторите Пароль')
+    
