30055300ea9e0a37badd5d0fb4748ea980a0b636
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-17 14:15:39 +0300

    some changes

--- ecommerce/shop/forms.py
+++ ecommerce/shop/forms.py
@@ -1,19 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class UserLogForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': _('Неверный Логин или Пароль')
+    }
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}), label='Логин')
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль',)
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Ваш Email'}))
 
 
 class UserRegForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': _('Пароли Не совпадают')
+    }
     model = User
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Логин'}), label='Логин')
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Ваш Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль')
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль',)
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Повторите Пароль'}),
                                 label='Повторите Пароль')
 
