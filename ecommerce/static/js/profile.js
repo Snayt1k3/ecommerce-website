643056a70474958298d746941abfca44d6260920
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-20 19:37:20 +0300

    add button

--- ecommerce/static/js/profile.js
+++ ecommerce/static/js/profile.js
@@ -1,11 +1,27 @@
 var btn = document.querySelector('.show_all_history');
-var body = document.querySelector('.history_orders')
+var btn1 = document.querySelector('.show_all_chistory');
+var body = document.querySelector('.history_orders');
+var body1 = document.querySelector('.current_orders');
+
+
 btn.addEventListener('click', function(){
     if (btn.innerHTML === 'Скрыть Историю'){
         btn.innerHTML = 'Показать Всю Историю';
         body.style.maxHeight = '500px';
     } else {
+
         btn.innerHTML = 'Скрыть Историю';
         body.style.maxHeight = 'None';
+    };
+});
+
+
+btn1.addEventListener('click', function(){
+    if (btn1.innerHTML === 'Скрыть Историю'){
+        btn1.innerHTML = 'Показать Всю Историю';
+        body1.style.maxHeight = '500px';
+    } else {
+        btn1.innerHTML = 'Скрыть Историю';
+        body1.style.maxHeight = 'None';
     };
 });
