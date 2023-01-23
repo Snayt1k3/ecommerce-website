3dd0b9095d1c108858c4ebf8d5706cd390dad18d
Snayt1k3 <snayt1k3twitch@gmail.com>
2023-01-23 12:56:24 +0300

    add stat model

--- ecommerce/profile_user/models.py
+++ ecommerce/profile_user/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from shop.models import Product
+
+
 # Create your models here.
+
+class SellerStatistics(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    add_cart = models.IntegerField()
+    remove_cart = models.IntegerField()
+    add_wish_list = models.IntegerField()
+    remove_wish_list = models.IntegerField()
+    bought = models.IntegerField()
